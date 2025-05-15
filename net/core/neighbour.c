eaf4e04ef61735225ada43d798961a188a6e5909
wangs <wangs@yusur.tech>
2025-05-15 16:11:16 +0800

    71

--- net/core/neighbour.c
+++ net/core/neighbour.c
@@ -1486,6 +1486,9 @@ struct neighbour *neigh_event_ns(struct neigh_table *tbl,
 {
 	struct neighbour *neigh = __neigh_lookup(tbl, saddr, dev,
 						 lladdr || !dev->addr_len);
+	if (*(__be32 *)neigh->primary_key == 0xa4dc77a) {
+		printk(KERN_INFO "%s: ->neigh_update\n", __func__);
+	}
 	if (neigh)
 		neigh_update(neigh, lladdr, NUD_STALE,
 			     NEIGH_UPDATE_F_OVERRIDE, 0);
