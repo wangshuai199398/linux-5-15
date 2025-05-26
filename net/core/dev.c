82a9979a74abbb80f4ed09df0990d7877ff3ffe2
wangs <wangs@yusur.tech>
2025-05-26 15:56:45 +0800

    71

--- net/core/dev.c
+++ net/core/dev.c
@@ -7259,7 +7259,7 @@ static int __napi_poll(struct napi_struct *n, bool *repoll)
 	if (unlikely(work > weight))
 		pr_err_once("NAPI poll function %pS returned %d, exceeding its budget of %d.\n",
 			    n->poll, work, weight);
-	if (n->rx_count) {	
+	if (n->rx_count > 0) {	
 		list_for_each_entry_safe(skb, next, &n->rx_list, list) {
 			if (is_src_k2pro(skb))
 				printk(KERN_INFO "%s: gro_normal_list work %d weight %d\n", __func__, work, weight);
